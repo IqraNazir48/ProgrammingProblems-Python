697d31db25959de8f5fa08b5c2f3467063498043
IqraNazir48 <130184671+IqraNazir48@users.noreply.github.com>
2025-08-17 01:15:59 +0000

    Add MultiplyStrings

---
+++ MultiplyStrings.py
@@ -0,0 +1,15 @@
+def multiply( num1, num2):
+    """
+    :type num1: str
+    :type num2: str
+    :rtype: str
+    """
+    num1=int(num1)
+    num2=int(num2)
+    product=num1*num2
+    return str(product)
+
+str1="6"
+str2="3"
+print(multiply(str1,str2))
+    
