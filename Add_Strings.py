1565c0c56f51f002775d8a5f1fb53f8b8f18d31b
IqraNazir48 <130184671+IqraNazir48@users.noreply.github.com>
2025-09-02 11:21:26 +0000

    Add Add_Strings

---
+++ Add_Strings.py
@@ -0,0 +1,13 @@
+def addStrings(num1, num2):
+    """
+    :type num1: str
+    :type num2: str
+    :rtype: str
+    """
+    return str(int(num1)+int(num2))
+
+
+num1="11"
+num2="123"
+print(addStrings(num1,num2))
+        
